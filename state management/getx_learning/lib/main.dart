d552a90adb5157bebb4b51864f611c115a3ef1a1
tickkie <tickkiesoloplay@gmail.com>
2025-06-23 17:18:31 +0700

    update 23/06/2025

--- state management/getx_learning/lib/main.dart
+++ state management/getx_learning/lib/main.dart
@@ -4,17 +4,18 @@ import 'package:get_storage/get_storage.dart';
 import 'package:getx_learning/bindings/home_binding.dart';
 import 'package:getx_learning/home.dart';
 import 'package:getx_learning/login.dart';
+import 'package:getx_learning/routes/routes.dart';
 import 'package:getx_learning/second_screen.dart';
 
 void main() async {
   await GetStorage.init();
   runApp(
     GetMaterialApp(
-      initialRoute: '/',
+      initialRoute: Routes.main,
       getPages: [
-        GetPage(name: '/', page: () => Login()),
-        GetPage(name: '/home', page: () => Home(), binding: HomeBinding()),
-        GetPage(name: '/second', page: () => SecondScreen()),
+        GetPage(name: Routes.main, page: () => Login()),
+        GetPage(name: Routes.home, page: () => Home(), binding: HomeBinding()),
+        GetPage(name: Routes.second, page: () => SecondScreen()),
       ],
     ),
   );
---
+++ state management/getx_learning/lib/routes/routes.dart
@@ -0,0 +1,5 @@
+class Routes {
+  static String main = '/';
+  static String home = '/home';
+  static String second = '/second';
+}
